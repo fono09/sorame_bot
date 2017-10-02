95e40a9720fa7227d762ea62c5c149bb75690acf
fono09 <fono@fono.jp>
2017-10-02 15:47:52 +0900

    Fix impots

--- bot.go
+++ bot.go
@@ -3,8 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
-	"config"
-	"model"
+	"github.com/fono09/sorame_bot/config"
+	"github.com/fono09/sorame_bot/model"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"log"
